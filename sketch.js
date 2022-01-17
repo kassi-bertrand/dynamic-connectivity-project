087df681c589725d86b21ca9a615c24fc4c1c1c1
Billy Bertrand <bertran.billy@gmail.com>
2022-01-17 13:24:47 -0600

    structured sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let network;
+
 function setup(){
-        
+    createCanvas(windowWidth, windowHeight)
+    network = new Network();
 }
 
 function draw(){
+    //update network
+    //display current state of network
+}
 
+//called once after a mouse click
+function mouseClicked(){
+    //adds a new node to the network
+}
+
+//called once after a mouse double-click
+function doubleClicked(){
+    //attempts a connection between two randomly chosen node in the network.
 }
